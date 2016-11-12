b53a681c6213bdb72cfb0fe91857137d607c47d4
BeccaLiu <rliu@enphaseenergy.com>
2016-11-11 19:54:56 -0800

    longest SubString

---
+++ src/Linear/LongestSubString.java
@@ -0,0 +1,38 @@
+package Linear;
+
+import java.util.HashSet;
+
+/**
+ * Created by rliu on 11/11/16.
+ * Longest Substring Without Repeating Characters
+ * Given a string, find the longest substring without repeating character and return the length of this substring
+ * bcacex -> 4
+ * bbaaae -> 2
+ */
+public class LongestSubString {
+    public static int longestSub(String s) {
+        HashSet<Character> hs = new HashSet<>();
+        int start = 0;
+        int end = 0;
+        int max = Integer.MIN_VALUE;
+        while (end < s.length()) {
+            char curr = s.charAt(end++);
+            if (hs.contains(curr)) {
+                while (s.charAt(start++) != curr) {
+                }
+            } else {
+                hs.add(curr);
+                if ((end - start) > max)
+                    max = end - start;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestSub("bcacexg"));
+        System.out.println(longestSub("bbaaaea"));
+        System.out.println(longestSub("abcdeff"));
+
+    }
+}
