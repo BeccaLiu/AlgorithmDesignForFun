028448860b7e6073ed1b73a962f7ea50b2ff909f
BeccaLiu <rliu@enphaseenergy.com>
2016-11-12 20:44:46 -0800

    Three sum

--- src/Linear/QueuePointer/ThreeSum.java
+++ src/Linear/QueuePointer/ThreeSum.java
@@ -1,11 +1,13 @@
 package Linear.QueuePointer;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Random;
 import java.util.stream.IntStream;
 
 /**
  * Created by rliu on 11/10/16.
+ * Three Sum with duplicate input and unsorted
  * Obviously brute force method is O(N^3)
  */
 public class ThreeSum {
@@ -33,6 +35,28 @@ public class ThreeSum {
         return false;
     }
 
+    public static int[] threeSum2(int[] a, int target) {
+        if (a.length < 3)
+            return null;
+        int[] rt = new int[3];
+        for (int i = 0; i < a.length; i++) {
+            int remind = target - a[i];
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int j = i + 1; j < a.length; j++) {
+                if (map.containsKey(a[j])) {
+                    rt[0] = i;
+                    rt[1] = j;
+                    rt[2] = map.get(a[j]);
+                    return rt;
+                } else {
+                    map.put(remind - a[j], j);
+                }
+            }
+        }
+        return rt;
+
+    }
+
     public static void main(String[] args) {
         int size = 10;
         int[] arr = new int[size];
@@ -40,6 +64,6 @@ public class ThreeSum {
         IntStream.range(0, size).forEach(i -> arr[i] = random.nextInt(10));
         System.out.println(Arrays.toString(arr));
         System.out.println(threeSum(arr, 5));
-
+        System.out.println(Arrays.toString(threeSum2(arr, 5)));
     }
 }
