2a1802ec3ef265ed12fa0fb828d67706822978d4
BeccaLiu <rliu@enphaseenergy.com>
2016-11-20 01:05:17 -0800

    trapping water using stack

--- Linear/TrappingInRainWater.java
+++ Linear/TrappingInRainWater.java
@@ -1,25 +1,57 @@
 package Linear;
 
+import java.util.ArrayDeque;
+
 /**
  * Created by rliu on 11/12/16.
  * Trapping rain water
  * Given n non-negative integers to represent the histogram bar height, the width of each bar is 1
  * Calculate how much water it can trap after raining
- *
  */
 public class TrappingInRainWater {
-    public static void primitiveIdea(int[] a){
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<a.length;i++){ //every iteration calculate the rain water start from i+1
-            int boundary=i+1;
-            while(a[boundary]<a[i])
-                boundary++;
-
-
+    //using two pointer
+    public static int primitiveIdea(int[] a) {
+        int water = 0;
+        for (int curr = 0; curr < a.length; curr++) {
+            int i = curr;
+            int left = curr;
+            int right = curr;
+            while (i >= 0)
+                if (a[i--] > a[left])
+                    left = i + 1;
+            i = curr;
+            while (i < a.length)
+                if (a[i++] > a[right])
+                    right = i - 1;
+            System.out.println(left + "," + right + ":" + (Math.min(a[left], a[right]) - a[curr]));
+            water += Math.min(a[left], a[right]) - a[curr];
         }
+        return water;
     }
+
+    //scan from beginning, and until meet the same height or height than the beginning, we know that we can calculate
+    //still two boundary, one is left wall, we store it in stack, and one is the right wall, we using curr pointer
+    //when the curr is less than stack.peek, we push it into stack
+    //when the curr is more than stack.peek, we know we can calculate now
+    public static int stackIdea(int[] a) {
+        int water = 0;
+        ArrayDeque<Integer> leftBounds = new ArrayDeque<>();
+        for (int curr = 0; curr < a.length; curr++) {
+            while (!leftBounds.isEmpty() && a[leftBounds.peek()] < a[curr]) {
+                int c = leftBounds.pop();
+                if (!leftBounds.isEmpty()) {
+                    int j = leftBounds.peek();
+                    water += (curr - j - 1) * (Math.min(a[j], a[curr]) - a[c]);
+                }
+            }
+            leftBounds.push(curr);
+        }
+        return water;
+    }
+
     public static void main(String[] args) {
-        int[] b = {5, 3, 4, 6, 7, 3};
+        int[] b = {3, 1, 2, 3, 1, 1, 2, 1};
+        System.out.print(stackIdea(b));
 
     }
 }
