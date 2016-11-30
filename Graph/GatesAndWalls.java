38f46cfafd3c1ff021b4307da2df9e72f64e42ac
BeccaLiu <rliu@enphaseenergy.com>
2016-11-29 23:25:32 -0800

    gates

--- Graph/GatesAndWalls.java
+++ Graph/GatesAndWalls.java
@@ -26,7 +26,8 @@ public class GatesAndWalls {
                 {Integer.MAX_VALUE, -1, Integer.MAX_VALUE, -1},
                 {0, -1, Integer.MAX_VALUE, Integer.MAX_VALUE,}
         };
-        bfsStartWithDoor(gw);
+        //bfsStartWithDoor(gw);
+        mutiEndBFS(gw);
         for (int i = 0; i < gw.length; i++) {
             for (int j = 0; j < gw[0].length; j++) {
                 System.out.print(String.format("%3d", gw[i][j]));
@@ -69,6 +70,44 @@ public class GatesAndWalls {
                 }
             }
         }
+    }
+
+    public static void mutiEndBFS(int[][] gw) {
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        for (int i = 0; i < gw.length; i++) {
+            for (int j = 0; j < gw[0].length; j++) {
+                if (gw[i][j] == 0)
+                    queue.add(new int[]{i, j});
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = queue.remove();
+                int x = curr[0];
+                int y = curr[1];
+                if (x - 1 >= 0 && gw[x - 1][y] > 0 && gw[x][y] + 1 < gw[x - 1][y]) {
+                    gw[x - 1][y] = gw[x][y] + 1;
+                    queue.add(new int[]{x - 1, y});
+                }
+                if (x + 1 < gw.length && gw[x + 1][y] > 0 && gw[x][y] + 1 < gw[x + 1][y]) {
+                    gw[x + 1][y] = gw[x][y] + 1;
+                    queue.add(new int[]{x + 1, y});
+                }
+                if (y - 1 >= 0 && gw[x][y - 1] > 0 && gw[x][y] + 1 < gw[x][y - 1]) {
+                    gw[x][y - 1] = gw[x][y] + 1;
+                    queue.add(new int[]{x, y - 1});
+                }
+                if (y + 1 < gw[0].length && gw[x][y + 1] > 0 && gw[x][y] + 1 < gw[x][y + 1]) {
+                    gw[x][y + 1] = gw[x][y] + 1;
+                    queue.add(new int[]{x, y + 1});
+
+                }
+
+            }
+        }
+
     }
 
 }
