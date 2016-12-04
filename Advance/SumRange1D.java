a77d1e9ae22314568e5cd8b7dea31ddb91662258
BeccaLiu <rliu@enphaseenergy.com>
2016-12-04 12:51:18 -0800

    sum range

--- Advance/SumRange1D.java
+++ Advance/SumRange1D.java
@@ -2,11 +2,81 @@ package Advance;
 
 /**
  * Created by rliu on 11/30/16.
+ * Range sum query
+ * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+ * Example:
+ * Given nums = [-2, 0, 3, -5, 2, -1]
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * Note:
+ * You may assume that the array does not change.
+ * There are many calls to sumRange function.
  */
 public class SumRange1D {
-    public static void main(String[] args) {
+    SegmentTreeNode root;
 
+    public SumRange1D(int[] arr) {
+        root = buildTree(arr, 0, arr.length - 1);
     }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9};
+        SumRange1D sumRange1D = new SumRange1D(arr);
+        System.out.println(sumRange1D.sumRange(2, 3));
+        sumRange1D.update(9, 6);
+        System.out.println(sumRange1D.sumRange(2, 3));
+    }
+
+    public static void update(SegmentTreeNode root, int i, int val) {
+        if (root.start == root.end) {
+            root.sum = val;
+            return;
+        }
+        int mid = root.start + (root.end - root.start) / 2;
+        if (i <= mid) {
+            update(root.left, i, val);
+        } else {
+            update(root.right, i, val);
+        }
+        root.sum = root.left.sum + root.right.sum;
+        return;
+    }
+
+    public SegmentTreeNode buildTree(int[] arr, int start, int end) {
+        if (start > end)
+            return null;
+        SegmentTreeNode root = new SegmentTreeNode(start, end);
+        if (start == end) {
+            root.sum = arr[end];
+        } else {
+            int mid = start + (end - start) / 2;
+            root.left = buildTree(arr, start, mid);
+            root.right = buildTree(arr, mid + 1, end);
+            root.sum = root.left.sum + root.right.sum;
+        }
+        return root;
+    }
+
+    public void update(int i, int val) {
+        update(root, i, val);
+    }
+
+    public int sumRange(int i, int j) {
+        return sumRange(root, i, j);
+    }
+
+    //kazaizhechaojiu
+    public int sumRange(SegmentTreeNode root, int i, int j) {
+        if (i > j)
+            return 0;
+        if (i == root.start && j == root.end)
+            return root.sum;
+        int mid = root.start + (root.end - root.start) / 2;
+        int rt = sumRange(root.left, i, mid) + sumRange(root.right, mid + 1, j);
+        return rt;
+    }
+
 
     //Primitive : 1.sumRange() O(n) add up all number update O(1)
     //            2.sumRange() O(1) using sum all number like SumRange(1,3)=sum[3]-sum[1]   update() O(n) :modified one number, need to add to all sum
@@ -15,15 +85,20 @@ public class SumRange1D {
     // Segment Tree
 
     //线段树
-    class SegmentTreeNode {
+    static class SegmentTreeNode {
         int start;
         int end;
         SegmentTreeNode left;
         SegmentTreeNode right;
-        int val;
+        int sum;
 
+        SegmentTreeNode(int start, int end) {
+            this.start = start;
+            this.end = end;
+            this.left = null;
+            this.right = null;
+            this.sum = 0;
+        }
     }
-
-    //get clue from segment tree, we will think of using binary index tree
 
 }
---
+++ Advance/SumRange1DBinaryIndexTree.java
@@ -0,0 +1,63 @@
+package Advance;
+
+/**
+ * Created by rliu on 12/4/16.
+ * Sum range Query
+ * get clue from segment tree, we will think of using binary index tree
+ * https://www.topcoder.com/community/data-science/data-science-tutorials/binary-indexed-trees/#2d
+ * We often need some sort of data structure to make our algorithms faster, this structure was first used for data compression.
+ * Now it is often used for storing frequencies and manipulating cumulative frequency tables.
+ */
+public class SumRange1DBinaryIndexTree {
+    int[] arr;
+    int[] biTree;
+
+    public SumRange1DBinaryIndexTree(int[] arr) {
+        this.arr = arr;
+        this.biTree = new int[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            init(i, arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+        SumRange1DBinaryIndexTree bit = new SumRange1DBinaryIndexTree(arr);
+        System.out.println(bit.sum(1, 4));
+        bit.update(2, 0);
+        System.out.println(bit.sum(1, 4));
+    }
+
+    public void update(int i, int val) {
+        int diff = arr[i] - val;
+        arr[i] = val;
+        init(i + 1, -diff);
+    }
+
+    public int sum(int i, int j) {
+        return getSum(j) - getSum(i - 1);
+
+    }
+
+    public void init(int i, int val) {
+        //j&-j: when j=1, j&-j=1 ,so 1+1=2 is the parent of 1;
+        //      when j=2, j&-j=2 ,so 2+2=4 is the parent of 2;
+        //      when j=4, j&-j=4, so 4+4=8 is the parent of 4;
+        //j&-j record the step that current index need to move to the right to get the parent which store the sum of current index
+        for (int j = i + 1; j < biTree.length; j += (j & (-j))) {
+            biTree[j] += val;
+        }
+    }
+
+    public int getSum(int j) {
+        //ex: when i=5, using i&-i we get 1, so the index 5-1=4 is the where sum of all the previous sum stored
+        //    when i=6, we can get 2, so index 6-2=4 is also where the sum of all the previous sum stored;
+        int sum = 0;
+        for (int i = j + 1; i > 0; i -= (i & -i)) {
+            sum += biTree[i];
+        }
+        return sum;
+    }
+
+
+}
