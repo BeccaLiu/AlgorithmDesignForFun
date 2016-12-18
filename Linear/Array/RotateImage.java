d3e1c2f66e6625fca0bc32cd8cb4289b1297d1ae
BeccaLiu <rliu@enphaseenergy.com>
2016-12-18 14:47:57 -0800

    array rotate image

---
+++ Linear/Array/RotateImage.java
@@ -0,0 +1,36 @@
+package Linear.Array;
+
+/**
+ * Created by rliu on 12/18/16.
+ * Rotate Image
+ * You are given an n x n 2D matrix representing an image.
+ * Rotate the image by 90 degrees (clockwise).
+ * <p>
+ * Follow up:
+ * Could you do this in-place?
+ */
+public class RotateImage {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}};
+        rotate(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++)
+                System.out.print(matrix[i][j]);
+            System.out.println();
+        }
+    }
+
+    //key is to find the relationship
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length - 1;
+        for (int i = 0; i <= n / 2; i++) {
+            for (int j = i; j < n - i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j][i];
+                matrix[n - j][i] = matrix[n - i][n - j];
+                matrix[n - i][n - j] = matrix[j][n - i];
+                matrix[j][n - i] = temp;
+            }
+        }
+    }
+}
