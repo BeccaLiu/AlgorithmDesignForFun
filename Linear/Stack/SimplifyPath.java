0d08f6abbb19008b15279011c8602010aefad6ef
BeccaLiu <rliu@enphaseenergy.com>
2016-12-28 09:12:01 -0800

    refactor

--- Linear/Stack/SimplifyPath.java
+++ Linear/Stack/SimplifyPath.java
@@ -14,6 +14,7 @@ public class SimplifyPath {
 
         System.out.println(simplePath("/tmp/cg/../algo/./../etc"));
         System.out.println(simplePathTwoPointer("/tmp/cg/../algo/./../etc"));
+        System.out.println(simplifyPath("/...."));
     }
 
     public static String simplePath(String s) {
@@ -56,5 +57,38 @@ public class SimplifyPath {
             }
         }
         return sb.toString();
+    }
+
+    public static String simplifyPath(String path) {
+        if (path == null || path.length() == 0)
+            return "";
+
+        //[I am stuck here]: try to combine split and parse the content all at once by writing my own split function, but it does not work so well, and take me a lot of time to debug
+        //so in some cases it may be easy to using just the java function, some cases it might be easy to using the a self write function.
+        ArrayDeque<String> stack = new ArrayDeque<String>();
+        int index = 0;
+        for (String s : path.split("/")) {
+            //pay attention to substring when index==i, string.substring(0,0), it will be ""
+            //but string.substring(0,0)=="" will be false; should use equals()
+            if (s.equals("") || s.equals("."))
+                continue;
+            if (s.equals("..")) {
+                if (!stack.isEmpty())
+                    stack.pop();
+            } else {
+                stack.push(s);
+            }
+        }
+
+        //corner case: ///  /....   /../ /home//dir
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append("/");
+            sb.append(stack.removeLast());
+        }
+        if (sb.length() == 0) {
+            return "/";
+        } else
+            return sb.toString();
     }
 }
