86d19eb6e7851d7b8bcacfa90cae2fd59b948588
BeccaLiu <rliu@enphaseenergy.com>
2017-01-11 17:35:59 -0800

    pow

---
+++ Tree/BinarySerach/Pow.java
@@ -0,0 +1,40 @@
+package Tree.BinarySerach;
+
+/**
+ * Created by rliu on 1/11/17.
+ * Implement pow(x, n).
+ * Key: n can be negative and positive, as well as x
+ */
+public class Pow {
+    public static void main(String[] args) {
+        System.out.println(myPow(34.00515, -3));
+        // System.out.println(myPow(0.00001, 2147483647));
+        //System.out.println(myPow(0,2147483647));
+        System.out.println(3 >> 1);
+        System.out.println((-3 / 2));
+    }
+
+    public static double myPow(double x, int n) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return x;
+        if (n == -1)
+            return 1.0 / x;
+
+        //do not use n>>1 as for 3>>1=1 but for -3>>1 = -2
+        int mid = n / 2;
+        //this is not using the idea of binary split, cause what we want to do is find the clue and remove redudent work
+        //return myPow(x,mid)*myPow(x,n-mid);
+        double partRs = myPow(x, mid);
+        //the return will be either partRs^2 or partRs^2*n
+        if (mid == n - mid)
+            return partRs * partRs;
+        else {
+            if (n < 0)
+                return partRs * partRs * myPow(x, -1);
+            else
+                return partRs * partRs * x;
+        }
+    }
+}
