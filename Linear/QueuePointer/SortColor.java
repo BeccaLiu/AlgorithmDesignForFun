b6271f118cd49f28a075cbfa44e6f8d8713b42d4
BeccaLiu <rliu@enphaseenergy.com>
2017-01-13 10:22:06 -0800

    adding bitwise answer

--- Linear/QueuePointer/SortColor.java
+++ Linear/QueuePointer/SortColor.java
@@ -37,12 +37,22 @@ public class SortColor {
         arr[j] = temp;
     }
 
-    //TODO:
+    //TODO: Java bitwise operation
+    //[<<]: add 0 to the rightmost digit  5<<2  0101->0100
+    //[>>]: add 0 to the leftmost digit  5>>2  0101->0001
+    //[>>>]: no sign , add sign to the leftmost digit
+    //       {5>>3==5>>>3,   0000 0000 0000 0000 0000 0000 0000 0101 ->  000 0000 0000 0000 0000 0000 0000 0000 0 5>>>3 = 0
+    //         but -5>>3     1111 1111 1111 1111 1111 1111 1111 1011 ->  000 1111 1111 1111 1111 1111 1111 1111 1 -5>>3 = -1 high-order bit 1 represent negative
+    //              5>>>3    1111 1111 1111 1111 1111 1111 1111 1011 ->  000 1111 1111 1111 1111 1111 1111 1111 1 -5>>>3 = 536870911
+    //[&] 0&0||0&1=0, 1&1=1;
+    //[|] 0|0=0, 1|0||1|1=1;
+    //[^] 0^1=1  0^0||1^1=0;
+    //[~] ~0=1 ~1=0;
     public static void xorSwap(int[] arr, int a, int b) {
         if (arr[a] != arr[b]) {
-            arr[a] ^= arr[b];
-            arr[b] ^= arr[a];
-            arr[a] ^= arr[b];
+            arr[a] ^= arr[b];  //store the diff between the ori arr[a] and arr[b] at arr[a];  arr[a]=diff arr[b]=b
+            arr[b] ^= arr[a]; //apply the diff to b will make it to a : arr[a]=diff arr[b]=a
+            arr[a] ^= arr[b]; //apply the diff to a will make it to b : arr[a]=b    arr[b]=a
         }
     }
 
