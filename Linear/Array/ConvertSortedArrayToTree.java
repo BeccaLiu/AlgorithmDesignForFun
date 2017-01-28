f3d24da547105ce4ed8758b24cf889f1e194edfd
BeccaLiu <rliu@enphaseenergy.com>
2017-01-27 22:51:41 -0800

    sorted convert to tree add comments

--- Linear/Array/ConvertSortedArrayToTree.java
+++ Linear/Array/ConvertSortedArrayToTree.java
@@ -17,13 +17,13 @@ public class ConvertSortedArrayToTree {
     }
 
     public static TreeNode sortedArrayToBST(int[] nums, int l, int r) {
-//        if(l>r)
-//            return null;
+        if (l > r)
+            return null;
         //using l==r will stackoverflow
         //as for 1,2,3,4,5,6 ->(0,5)mid=3  ->(0,1) mid=1 -> (0,-1) mid=-1, there might not be a chance that l==r,
         //when l=0,r=-1, we should return, but if you set l==r, mid=(0+1)/2=0 and next itr will be (0,-1) again it will not return the node you want
-        if (l == r)
-            return new TreeNode(l);
+//        if (l == r)
+//            return new TreeNode(l);
 
         int mid = l + (r - l) / 2;
         TreeNode root = new TreeNode(nums[mid]);
