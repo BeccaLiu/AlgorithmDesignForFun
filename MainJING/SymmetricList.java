6599f6af4e4fd087583991f329684511bc47e729
BeccaLiu <rliu@enphaseenergy.com>
2017-02-12 22:04:53 -0800

    symmetric

--- MainJING/SymmetricList.java
+++ MainJING/SymmetricList.java
@@ -6,6 +6,8 @@ import java.util.*;
  * Created by rliu on 2/11/17.
  * give you list of points, check if they are symmetric by x = number
  * point contains (x,y)
+ * 356. Line Reflection
+ * Given n points on a 2D plane, find if there is such a line parallel to y-axis that reflect the given points.
  */
 public class SymmetricList {
     public static void main(String[] args) {
@@ -24,6 +26,7 @@ public class SymmetricList {
         points.add(new Point(6, 1));
         points.add(new Point(7, 1));
         System.out.println(isSymmetricHash(points)); //true
+        System.out.println(isReflected());
     }
 
     public static boolean isSymmetric(List<Point> points) {
@@ -43,9 +46,9 @@ public class SymmetricList {
             @Override
             public int compare(Point o1, Point o2) {
                 if (o1.x != o2.x)
-                    return (int) (o1.x - o2.x);
+                    return o1.x - o2.x;
                 else
-                    return (int) (o1.x <= xLine ? (o1.y - o2.y) : (o2.y - o1.y));
+                    return o1.x <= xLine ? (o1.y - o2.y) : (o2.y - o1.y);
             }
         });
 
@@ -63,16 +66,16 @@ public class SymmetricList {
     public static boolean isSymmetricHash(List<Point> points) {
         if (points == null || points.size() == 0)
             return false;
-        double xMin = points.get(0).x;
-        double xMax = points.get(0).x;
+        int xMin = points.get(0).x;
+        int xMax = points.get(0).x;
         for (int i = 1; i < points.size(); i++) {
-            double currX = points.get(i).x;
+            int currX = points.get(i).x;
             if (currX < xMin)
                 xMin = currX;
             if (currX > xMax)
                 xMax = currX;
         }
-        double xLine = (xMax + xMin) / 2;
+        int xLine = (xMax + xMin) / 2;
 
         HashMap<Point, Integer> map = new HashMap<>(); //key is point, value is count
         for (int i = 0; i < points.size(); i++) {
@@ -100,11 +103,11 @@ public class SymmetricList {
     public static boolean isSymmetricHashBetter(List<Point> points) {
         if (points == null || points.size() == 0) return false;
         HashMap<Point, Integer> hashmap = new HashMap<>(); //key is point, value is count
-        double xMin = points.get(0).x;
-        double xMax = points.get(0).x;
+        int xMin = points.get(0).x;
+        int xMax = points.get(0).x;
         for (int i = 1; i < points.size(); i++) {
             Point curPoint = points.get(i);
-            double currX = curPoint.x;
+            int currX = curPoint.x;
             xMin = Math.min(xMin, currX);
             xMax = Math.max(xMax, currX);
             Integer number = hashmap.get(currX);
@@ -114,7 +117,7 @@ public class SymmetricList {
                 hashmap.put(curPoint, number + 1);
             }
         }
-        double xLine = (xMax + xMin) / 2;
+        int xLine = (xMax + xMin) / 2;
 
         for (Point p : hashmap.keySet()) {
             Point symmetriP = new Point(2 * xLine - p.x, p.y);
@@ -129,11 +132,39 @@ public class SymmetricList {
         return true;
     }
 
-    static class Point {
-        double x;
-        double y;
+    public static boolean isReflected() {
+        //int[][] points = new int[][]{{1, 1}, {2, 1}, {2, 2}, {3, 3}, {3, 3}, {4, 0}, {4, 1}, {5, 3}, {5, 3}, {6, 2}, {6, 1}, {7, 1}};
+        int[][] points = {{16, 1}, {-16, 1}, {-16, 1}}; //will return true
+        if (points == null || points.length == 0) return true;
+        HashMap<Point, Integer> hashmap = new HashMap<>();
+        int xMin = points[0][0];
+        int xMax = points[0][0];
+        for (int i = 0; i < points.length; i++) {
+            xMin = Math.min(xMin, points[i][0]);
+            xMax = Math.max(xMax, points[i][0]);
+            Point p = new Point(points[i][0], points[i][1]);
+            Integer count = hashmap.get(p);
+            if (count == null)
+                hashmap.put(p, 1);
+        }
+        double xLine = (double) (xMax + xMin) / 2;
 
-        public Point(double x, double y) {
+        for (Map.Entry<Point, Integer> cur : hashmap.entrySet()) {
+            int count = cur.getValue();
+            Point point = cur.getKey();
+            Integer symmetricCount = hashmap.get(new Point((int) (xLine * 2 - point.x), point.y));
+            if (symmetricCount == null || symmetricCount != count)
+                return false;
+        }
+
+        return true;
+    }
+
+    static class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
@@ -144,7 +175,7 @@ public class SymmetricList {
         }
 
         public int hashCode() {
-            return (int) (x * y);
+            return (x * y);
         }
     }
 }
