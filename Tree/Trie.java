851196385d5a3443c09a9ec0113b962a7d4b6701
BeccaLiu <rliu@enphaseenergy.com>
2017-02-16 14:21:54 -0800

    trie

--- Tree/Trie.java
+++ Tree/Trie.java
@@ -1,5 +1,7 @@
 package Tree;
 
+import java.util.ArrayDeque;
+
 /**
  * Created by rliu on 2/9/17.
  * 208. Implement Tree.Trie (Prefix Tree)
@@ -22,6 +24,8 @@ public class Trie {
         boolean param_3 = obj.search("app");
         System.out.println(param_2);
         System.out.println(param_3);
+        boolean rt = obj.searchWildCard(".pp");
+        System.out.println(rt);
     }
 
     /**
@@ -51,6 +55,38 @@ public class Trie {
             pre = pre.nodes[index];
         }
         return pre.isWord == null ? false : pre.isWord;
+    }
+
+    /**
+     * Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+     */
+    public boolean searchWildCard(String word) {
+        TireNode pre = root;
+        ArrayDeque<TireNode> queue = new ArrayDeque<>();
+        queue.offer(pre);
+        for (int i = 0; i < word.length() && !queue.isEmpty(); i++) {
+            int size = queue.size();
+            for (int j = 0; j < size; j++) {
+                TireNode curr = queue.poll();
+                char c = word.charAt(i);
+                if (c != '.') {
+                    if (curr.nodes[c - 'a'] != null)
+                        queue.offer(curr.nodes[c - 'a']);
+                } else {
+                    for (int k = 0; k < curr.nodes.length; k++) {
+                        if (curr.nodes[k] != null)
+                            queue.offer(curr.nodes[k]);
+                    }
+                }
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            TireNode curr = queue.poll();
+            if (curr.isWord)
+                return true;
+        }
+        return false;
     }
 
     /**
