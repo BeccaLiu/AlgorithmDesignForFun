4026921461e82c2ff38b5bb66b61b381d1628be0
BeccaLiu <rliu@enphaseenergy.com>
2017-03-12 12:16:00 -0700

    insertion sort

--- Linear/LinkedList/InsertSortedList.java
+++ Linear/LinkedList/InsertSortedList.java
@@ -7,7 +7,9 @@ package Linear.LinkedList;
  */
 public class InsertSortedList {
     public static void main(String[] args) {
+
         insertionSortList(new Node(new int[]{4, 3, 2, 1, 5, 0}));
+        System.out.println(insertionSortList2(new ListNode(new int[]{4, 3, 2, 1, 5, 0})));
     }
 
     public static Node insertionSortList(Node head) {
@@ -34,5 +36,28 @@ public class InsertSortedList {
         }
         return dummy.next;
 
+    }
+
+    public static ListNode insertionSortList2(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode curr = dummy.next;
+
+        while (curr.next != null) {
+            ListNode p = curr.next;
+            if (p.val >= curr.val) {
+                curr = curr.next;
+            } else {
+                //need to move and insert
+                curr.next = p.next;
+                ListNode tempPre = pre;
+                while (tempPre != curr && tempPre.next != curr && p.val > tempPre.next.val)
+                    tempPre = tempPre.next;
+                p.next = tempPre.next;
+                tempPre.next = p;
+            }
+        }
+        return dummy.next;
     }
 }
