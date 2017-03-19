17ddac448f72bf4b9d91bcc1fedbd8d8a78a4aee
BeccaLiu <rliu@enphaseenergy.com>
2017-03-18 18:32:09 -0700

    group segment

--- MainJING/GroupSegment.java
+++ MainJING/GroupSegment.java
@@ -41,14 +41,17 @@ public class GroupSegment {
 
             SegmentNode child = childs.get(segmentNames[i]);
             if (child == null) {
+                //nodes like FunStore, using increment id as id
                 if (i < segmentNames.length - 1)
                     child = new SegmentNode(id++, segmentNames[i]);
                 else {
+                    //nodes like Likes Clothes so much, using real id as id, and set the price.
                     child = new SegmentNode(segmentId, segmentNames[i]);
                     child.price = price;
                 }
                 childs.put(segmentNames[i], child);
             }
+            //moving to next level.
             curr = child;
         }
     }
@@ -56,27 +59,32 @@ public class GroupSegment {
 
     String getCSVContent() {
         StringBuilder sb = new StringBuilder();
+        //print all the child of root, like segment node start with FunStore, Target
         for (SegmentNode node : root.childs.values()) {
             List<List<String>> res = new ArrayList<>();
+            //DFS save all the info in res
             dfsPrint(node, new ArrayList<String>(), res);
             for (List<String> l : res) {
                 sb.append(String.join(",", l));
                 sb.append("\n");
             }
-
         }
         return sb.toString();
     }
 
     void dfsPrint(SegmentNode node, List<String> prev, List<List<String>> res) {
         prev.add(node.nodeName);
+        //grounping current level information
         List<String> curr = new ArrayList<>();
         curr.add(Integer.toString(node.id));
         curr.add(String.join(">", prev));
         if (node.price != null)
             curr.add(Integer.toString(node.price));
 
+        //add current level info to res
         res.add(curr);
+
+        //go to the next level
         for (SegmentNode c : node.childs.values()) {
             dfsPrint(c, prev, res);
         }
