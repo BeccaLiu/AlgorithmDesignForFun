5fd8d7a57e21a0d940360992916db0b595143e4d
BeccaLiu <rliu@enphaseenergy.com>
2017-04-16 10:45:01 -0700

    using lambda for comparator

--- Heap/FindMedianFromDataStream.java
+++ Heap/FindMedianFromDataStream.java
@@ -1,6 +1,5 @@
 package Heap;
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -12,12 +11,7 @@ public class FindMedianFromDataStream {
 
     FindMedianFromDataStream() {
         lg = new PriorityQueue<Integer>();
-        sm = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2 - o1;
-            }
-        });
+        sm = new PriorityQueue<>((o1, o2) -> o2 - o1);
     }
 
     public static void main(String[] args) {
